b0c6215fd0d52afb51f2f56b6e9eeb65cf7d9447
Aazim Haque <aazim.haque@gmail.com>
2018-11-22 16:17:26 +0400

    Check for last coords and move if not changed

--- mousemover.py
+++ mousemover.py
@@ -1,5 +1,15 @@
 import pyautogui, time
 
+currentX = 0
+currentY = 0
+lastX = 0
+lastY = 0
+
 while True:
-    time.sleep(5)
-    pyautogui.moveRel(0,10)
+    time.sleep(15)
+    currentX, currentY = pyautogui.position()
+    if currentX != lastX or currentY != lastY:
+        lastX = currentX
+        lastY = currentY
+    else:
+        pyautogui.moveRel(0,10)
